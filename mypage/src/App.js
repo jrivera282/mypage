fe0206b38ce7579950032b53dd035e1c3ecfce5a
jrivera282 <55336576+jrivera282@users.noreply.github.com>
2021-05-27 22:23:04 -0400

    added some components
    added compoments to render the github page ghpages

--- mypage/src/App.js
+++ mypage/src/App.js
@@ -2,10 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 import { HashRouter, Route, Link } from "react-router-dom"; 
+import { render } from '@testing-library/react';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+      <HashRouter basename='/'>
+    <div>
+      <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      </ul>
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+    </HashRouter>
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,8 +32,10 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
+    </div>*/
+    );
+  }
+const Home = () => <div><h2>Home</h2></div>
+const About = () => <div><h2>About</h2></div>
 
 export default App;
